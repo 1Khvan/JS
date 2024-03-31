b23e6b6a70934c884dd86b79086e9b00a04ee46f
Igor Khvan <>
2024-03-31 13:53:46 +0300

    Part2

---
+++ Part2/Practice11/data.js
@@ -0,0 +1,26 @@
+let dataProductsJSON = `[
+    {
+        "image" : "1.jpg",
+        "title" : "MANGO  PEOPLE  T-SHIRT",
+        "price" : "$300",
+        "color" : "Red",
+        "size" : "Xl",
+        "quantity" : "2"
+    },
+    {
+        "image" : "2.jpg",
+        "title" : "ELLERY X M'O CAPSULE",
+        "price" : "$52",
+        "color" : "Black",
+        "size" : "Xl",
+        "quantity" : "1"
+    },
+    {
+        "image" : "3.jpg",
+        "title" : "NICE MAN CLOTHING",
+        "price" : "$502",
+        "color" : "Nuded",
+        "size" : "Xl",
+        "quantity" : "3"
+    }
+]`;
